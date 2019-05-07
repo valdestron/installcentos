cdd1449e2860f373161cdeefa99751d5468572d3
Valdas Mazrimas <valdas.mazrimas@gmail.com>
2019-05-07 09:30:44 +0300

    Add cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+yum -y remove openshift openshift-* etcd docker
+rm -rf /etc/openshift /var/lib/openshift /etc/etcd \
+    /var/lib/etcd /etc/sysconfig/openshift* /etc/sysconfig/docker* \
+    /root/.kube/config /etc/ansible/facts.d /usr/share/openshift
+yum -y remove ansible
+yum -y remove docker docker-common docker-selinux docker-engine 
