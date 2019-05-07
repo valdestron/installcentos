192589f7d69130695dd8068348bbea243081e492
Valdas Mazrimas <valdas.mazrimas@gmail.com>
2019-05-07 10:26:15 +0300

    Update

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 yum -y remove openshift openshift-* etcd docker
+rm -rf ~/.ssh/authorized_keys ~/.ssh/id_rsa ~/.ssh/id_rsa.pub
 rm -rf /etc/openshift /var/lib/openshift /etc/etcd \
     /var/lib/etcd /etc/sysconfig/openshift* /etc/sysconfig/docker* \
     /root/.kube/config /etc/ansible/facts.d /usr/share/openshift
 yum -y remove ansible
-yum -y remove docker docker-common docker-selinux docker-engine 
+yum -y remove docker docker-common docker-selinux docker-engine
--- install-openshift.sh
+++ install-openshift.sh
@@ -142,7 +142,7 @@ systemctl enable docker
 if [ ! -f ~/.ssh/id_rsa ]; then
 	ssh-keygen -q -f ~/.ssh/id_rsa -N ""
 	cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-	ssh -o StrictHostKeyChecking=no root@$IP "pwd" < /dev/null
+	ssh -o StrictHostKeyChecking=no root@$IP "pwd"
 fi
 
 export METRICS="True"
